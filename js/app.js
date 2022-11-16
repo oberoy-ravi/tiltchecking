06e50463d60676dcf85dd89403cab35a4420daea
oberoy-ravi <114639806+oberoy-ravi@users.noreply.github.com>
2022-11-16 15:33:15 +0530

    Add files via upload

---
+++ js/app.js
@@ -0,0 +1,124 @@
+/*
+   *********************************************************************JH@ ⌣ @YUSH**************************************************************************************
+*/
+
+
+
+particlesJS('particles-js',
+  
+{
+  "particles": {
+    "number": {
+      "value": 48,
+      "density": {
+        "enable": true,
+        "value_area": 400.6025061692789
+      }
+    },
+    "color": {
+      "value": "#b8a228"
+    },
+    "shape": {
+      "type": "circle",
+      "stroke": {
+        "width": 4,
+        "color": "#b8a228"
+      },
+      "polygon": {
+        "nb_sides": 4
+      },
+      "image": {
+        "src": "img/github.svg",
+        "width": 50,
+        "height": 50
+      }
+    },
+    "opacity": {
+      "value": 0.5,
+      "random": false,
+      "anim": {
+        "enable": false,
+        "speed": 1,
+        "opacity_min": 0.1,
+        "sync": false
+      }
+    },
+    "size": {
+      "value": 4.006,
+      "random": true,
+      "anim": {
+        "enable": true,
+        "speed": 14.607,
+        "size_min": 10.549,
+        "sync": true
+      }
+    },
+    "line_linked": {
+      "enable": true,
+      "distance": 128.192,
+      "color": "#fff",
+      "opacity": 0.4,
+      "width": 2.563
+    },
+    "move": {
+      "enable": true,
+      "speed": 1.9,
+      "direction": "none",
+      "random": false,
+      "straight": false,
+      "out_mode": "bounce",
+      "bounce": false,
+      "attract": {
+        "enable": true,
+        "rotateX": 600,
+        "rotateY": 1121.687017273981
+      }
+    }
+  },
+  "interactivity": {
+    "detect_on": "canvas",
+    "events": {
+      "onhover": {
+        "enable": true,
+        "mode": "grab"
+      },
+      "onclick": {
+        "enable": true,
+        "mode": "push"
+      },
+      "resize": true
+    },
+    "modes": {
+      "grab": {
+        "distance": 267.8027997565431,
+        "line_linked": {
+          "opacity": 1
+        }
+      },
+      "bubble": {
+        "distance": 400,
+        "size": 40,
+        "duration": 2,
+        "opacity": 8,
+        "speed": 3
+      },
+      "repulse": {
+        "distance": 81.15236356258882,
+        "duration": 0.4
+      },
+      "push": {
+        "particles_nb": 4
+      },
+      "remove": {
+        "particles_nb": 2
+      }
+    }
+  },
+  "retina_detect": true
+}
+
+);
+
+/*
+   *********************************************************************JH@ ⌣ @YUSH**************************************************************************************
+*/
